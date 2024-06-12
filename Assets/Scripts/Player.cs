0585c75ab85a4f4eb3d3d1bb3ceaba444b55f067
TranHuuMinhDuc <ytientvhue@gmail.com>
2024-06-12 20:48:47 +0700

    update Car

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _force = 10f;
+    private Rigidbody _rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            
+            Vector3 force = transform.forward * _force;
+         _rb.AddForce(force, ForceMode.VelocityChange);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag.Equals("Obstacle"))
+        {
+            Debug.Log("Player collided with" + other.gameObject.name);
+            Debug.Log("Player collided with" + other.impulse);
+            Debug.Log("Player collided with relative velocity" + other.relativeVelocity);
+            Debug.Log("Player collided with contact points" + other.contacts[0].point);
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.SetActive(false);
+        Destroy(other.gameObject, 1f);
+    }
+}
