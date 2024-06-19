608a03171840f7a9b08ae646c9fb8252f59bafe7
TranHuuMinhDuc <ytientvhue@gmail.com>
2024-06-19 20:39:50 +0700

    Update Water(Floating)

---
+++ Assets/Scripts/FloatingObject.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class FloatingObject : MonoBehaviour
+{
+    [SerializeField] private float underWaterDrag;
+    [SerializeField] private float underWaterAngularDrap;
+    [SerializeField] private float airDrag;
+    [SerializeField] private float airAngularDrap;
+    [SerializeField] private float waterPower;
+    [SerializeField] private Transform[] floatPoints;
+
+    private Rigidbody _rb;
+    private bool _isUnderWater;
+
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        int pointUnderWaterCount = 0;
+        foreach (var point in floatPoints)
+        {
+            var diff = point.position.y - 0;
+            if (diff < 0)
+            {
+                _rb.AddForceAtPosition(
+                    Vector3.up * waterPower * Mathf.Abs(diff),
+                    point.position,
+                    ForceMode.Acceleration);
+                pointUnderWaterCount++;
+                if (!_isUnderWater)
+                {
+                    _isUnderWater = true;
+                    SetStage(true);
+                }
+            }
+        }
+        if(_isUnderWater && pointUnderWaterCount == 0)
+        {
+            _isUnderWater = false;
+            SetStage(false);
+        }
+
+    }
+    private void SetStage(bool underWater)
+    {
+        if (underWater)
+        {
+            _rb.drag = underWaterDrag;
+            _rb.angularDrag = underWaterAngularDrap;
+        }
+        else
+        {
+            _rb.drag = airDrag;
+            _rb.angularDrag= airAngularDrap;
+        }
+    }
+}
+
